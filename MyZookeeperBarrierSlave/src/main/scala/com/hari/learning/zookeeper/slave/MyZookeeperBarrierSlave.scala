fc38edba8b65a0210063e23a8a578bd901815056
Harikrishna <krishari2006@gmail.com>
2019-06-15 11:02:08 +0530

    Adding first copy of MyZookeeperBarrierSlave

--- MyZookeeperBarrierSlave/src/main/scala/com/hari/learning/zookeeper/slave/MyZookeeperBarrierSlave.scala
+++ MyZookeeperBarrierSlave/src/main/scala/com/hari/learning/zookeeper/slave/MyZookeeperBarrierSlave.scala
@@ -4,13 +4,14 @@ import org.apache.zookeeper.{ ZooKeeper, Watcher, WatchedEvent }
 import org.apache.zookeeper.Watcher.Event.EventType._
 import java.util.concurrent.CountDownLatch
 
-class MyZookeeperBarrierSlave extends Runnable with Watcher {
+class MyZookeeperBarrierSlave(zkHost: String, zkPort: Int) extends Runnable with Watcher {
   var requiresInterruption: Boolean = false
   var latch: CountDownLatch = new CountDownLatch(1)
   val zkBarrierPathName = "/zk_barrier"
   override def run = {
     while (true) {
       Thread.sleep(1000)
+      println("Just printing")
       if (requiresInterruption) {
         latch.await()
       }
@@ -34,4 +35,14 @@ class MyZookeeperBarrierSlave extends Runnable with Watcher {
     }
   }
 
+}
+
+object MyZookeeperBarrierSlave {
+  import java.util.concurrent.Executors
+  def main(args: Array[String]): Unit = {
+    val zkHost = args(0)
+    val zkHostPort = args(1).toInt
+    val threadPoolExecutor = Executors.newFixedThreadPool(2)
+    threadPoolExecutor.submit(new MyZookeeperBarrierSlave(zkHost, zkHostPort))
+  }
 }
