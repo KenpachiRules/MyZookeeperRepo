0582edbbd5adfd0d221a68fe36bb4c657ad30b94
Harikrishna <krishari2006@gmail.com>
2019-06-17 21:33:01 +0530

    LocksCons is dev complete

--- locks/MyZookeeperLocksCons/src/main/scala/com/hari/learning/zookeeper/locks/MyZookeeperLocksCons.scala
+++ locks/MyZookeeperLocksCons/src/main/scala/com/hari/learning/zookeeper/locks/MyZookeeperLocksCons.scala
@@ -1,61 +1,75 @@
 package com.hari.learning.zookeeper.locks
-import org.apache.zookeeper.{ ZooKeeper, Watcher, WatchedEvent,CreateMode }
-import org.apache.zookeeper.data.{ACL,Id}
+import org.apache.zookeeper.{ ZooKeeper, Watcher, WatchedEvent, CreateMode }
+import org.apache.zookeeper.Watcher.Event.EventType._
+import org.apache.zookeeper.data.{ ACL, Id }
 import java.util.concurrent.{ Executors, CountDownLatch }
-import java.lang.Runnable
+import java.lang.{ Runnable, Comparable }
 import scala.collection.JavaConversions._
+import java.util.Collections
 
-class MyZookeeperLocksCons(zkHost: String, zkPort: Int,consCount:Int) extends Watcher with Runnable {
-  val zk: ZooKeeper = new ZooKeeper(zkHost + ":" + zkPort, 1000, this)
+class MyZookeeperLocksCons(zkHost: String, zkPort: Int, consCount: Int) extends Watcher with Runnable {
+  val zk: ZooKeeper = new ZooKeeper(zkHost + ":" + zkPort, 10000, this)
   val zkLockParent = "/zk_lock"
   val zkPathSep = "/"
-  val zkLockChild = "child"
-  val zkLockChildPrefix = "-" 
   var lock: CountDownLatch = null
   val lockStat = zk.exists(zkLockParent, false)
-  val aclList = new ACL(755,new Id("world","everyone"))::Nil
-  var count  = 0
-  var child:String = ""
-  if(lockStat == null ) // lock_node does not exist
-    zk.create(zkLockParent,null,aclList,CreateMode.PERSISTENT)
+  val aclList = new ACL(755, new Id("world", "everyone")) :: Nil
+  var count = 0
+  var child: String = ""
+  var prevNode: String = null
+  var childPos: Int = 0
+  if (lockStat == null) // lock_node does not exist
+    zk.create(zkLockParent, null, aclList, CreateMode.PERSISTENT)
   override def run: Unit = {
     while (true) {
       Thread.sleep(1000)
-      count +=1
-      if(count == 5){
+      count += 1
+      if (count == 5) {
         //create a znode under the lockParent node and check for if there are any smaller children.
-        child = zk.create(zkLockParent+zkPathSep+zkLockChild+zkLockChildPrefix
-            ,null,aclList,CreateMode.EPHEMERAL_SEQUENTIAL)
-        val createdChildPos = child.split("zkLockChildPrefix")(1).toInt
-        val children =  zk.getChildren(zkLockParent,true)
-        val smallestChild = children.map( str => (str,str.split(zkLockChildPrefix)(1).toInt)).toList(0)
-        if(createdChildPos <= smallestChild._2) {
-          // this is the smallest 
-        }else {
+        child = zk.create(zkLockParent + zkPathSep, null, aclList, CreateMode.EPHEMERAL_SEQUENTIAL)
+        val children = zk.getChildren(zkLockParent, true).asInstanceOf[scala.collection.immutable.List[String]]
+        Collections.sort(children)
+        childPos = children.indexOf(child)
+        if (childPos == 0) {
+          // this is the smallest and hence I am the owner of the lock
+          print
+        } else {
           // double check if the smallestChild exists
-          if(zk.exists(smallestChild._1, true) != null) {
+          if (zk.exists(children(childPos - 1), true) != null) {
+            prevNode = children(childPos - 1)
             lock = new CountDownLatch(1)
             lock.await
-          }
+          } else
+            print
         }
-        
+        count = 0
       }
     }
   }
-  
-  override def print():Unit = {
-    for(i <- 0 until 5) {
-      println(consCount +" is the owner of the lock ")
+
+  def print: Unit = {
+    for (i <- 0 until 5) {
+      println(consCount + " is the owner of the lock ")
       Thread.sleep(1000)
     }
     val myChild = zk.exists(child, this)
-    zk.delete(, myChild.)
+    zk.delete(child, myChild.getVersion)
   }
-  
+
   override def process(event: WatchedEvent): Unit = {
     event.getType match {
-      case 
-      
+      case NodeCreated => {
+        // not yet decided.
+      }
+      case NodeDeleted => {
+        if (event.getPath.equals(prevNode)) {
+          // release the lock
+          lock.countDown()
+        }
+      }
+      case NodeChildrenChanged => {}
+      case NodeDataChanged     => {}
+      case None                => {}
     }
   }
 
@@ -67,7 +81,8 @@ object MyZookeeperLocksCons {
     val zkHost = args(0)
     val zkHostPort = args(1).toInt
     val executors = Executors.newFixedThreadPool(2)
-    executors.submit(new MyZookeeperLocksCons(zkHost, zkHostPort))
+    val consCount = args(2).toInt
+    executors.submit(new MyZookeeperLocksCons(zkHost, zkHostPort, consCount))
   }
 
 }
